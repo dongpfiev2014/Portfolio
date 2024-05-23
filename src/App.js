05d7f1f880891ede0ec68ed16636ef9e57ee1c30
dongpfiev2014 <dongpfiev2014@gmail.com>
2024-05-24 00:53:49 +0700

    update

--- src/App.js
+++ src/App.js
@@ -7,17 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h3>Hello everyone. I'm Dong !!</h3>
-        <p>
-          I'm passionate about technology and aspire to become a Backend and
-          DevOps Developer. With a keen interest in the intricate workings of
-          software systems, I am driven to optimize backend functionalities and
-          streamline deployment processes. My goal is to contribute to the
-          development of robust and scalable applications while ensuring
-          seamless integration and efficient operations. With a strong
-          foundation in programming and a desire to delve deeper into
-          infrastructure management and automation, I am excited about the
-          opportunities ahead in the world of backend development and DevOps.
-        </p>
+        <p>Thanks for scrolling</p>
       </header>
     </div>
   );
